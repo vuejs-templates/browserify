6c0de5606611dbf4e292375aaf8c30d5974f0d81
zigomir <zigomir@gmail.com>
2016-08-12 21:15:10 -0700

    Vue 2.0 changes.
    - fix tests
    - eslint with standard

--- template/src/main.js
+++ template/src/main.js
@@ -2,6 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 
 new Vue({ // eslint-disable-line no-new
-  el: 'body',
+  el: '#app',
   render: (h) => h(App)
 })
--- template/test/unit/Hello.spec.js
+++ template/test/unit/Hello.spec.js
@@ -1,15 +1,13 @@
-/* global describe, it, expect */
-
 import Vue from 'vue'
 import Hello from '../../src/components/Hello.vue'
 
 describe('Hello.vue', () => {
   it('should render correct contents', () => {
     const vm = new Vue({
-      template: '<div><hello></hello></div>',
-      components: { Hello }
-    }).$mount()
-    expect(vm.$el.querySelector('.hello h1').textContent).toBe('Hello World!')
+      el: document.createElement('div'),
+      render: (h) => h(Hello)
+    })
+    expect(vm.$el.querySelector('.hello h1').textContent).toBe('Hello Vue!')
   })
 })
 
