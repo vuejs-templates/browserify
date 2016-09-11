a69bc34f00b420e907c6c95656734671bc38122f
Thorsten <t.luenborg@googlemail.com>
2016-09-11 22:26:33 +0200

    typo

--- template/src/main.js
+++ template/src/main.js
@@ -2,7 +2,7 @@
 // The following line loads the standalone build of Vue instead of the runtime-only build,
 // so you don't have to do: import Vue from 'vue/dist/vue'
 // (also, loading Vue standalone this way breaks vueify, so don't do it)
-// This is done with the traonsform "aliasify". For the config, see package.json
+// This is done with the transform "aliasify". For the config, see package.json
 {{/if_eq}}
 import Vue from 'vue'
 import App from './App.vue'
